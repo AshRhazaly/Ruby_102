440bdf25a0367f13d9a4dde64ced7c318f4a460f
Unknown <ashleighrhazaly@gmail.com>
2017-02-02 10:05:27 +0800

    Added a loop in the main ruby file for greater user experience

--- personal budget manager/main_app.rb
+++ personal budget manager/main_app.rb
@@ -1,5 +1,20 @@
 load 'interface_class.rb'
 
-ash = Interface.new
+loop do
+  puts "Hi there! Welcome to DBS!"
+  puts "Press 1 if you'd like to create a new account."
+  puts "Press 2 if you've got an existing account."
+  input = gets.chomp
 
-ash.load_data
+  if input == "1"
+    user = Interface.new
+    user.create_account
+    break
+  elsif input == "2"
+    user = Interface.new
+    user.load_data
+    break
+  else
+    break
+  end
+end
