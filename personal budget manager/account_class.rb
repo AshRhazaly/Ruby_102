696d216ffff635d8b02f6999a6b92e6019b73e0b
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-02 11:27:51 +0800

    Added the ability to view transaction history

--- personal budget manager/account_class.rb
+++ personal budget manager/account_class.rb
@@ -1,15 +1,18 @@
 class Account
-  attr_accessor :balance, :name
+  attr_accessor :balance, :name, :transactions
 
   def initialize(starting_balance, name)
     @balance = starting_balance
     @name = name
+    @transactions = []
+    @transactions_count = 0
   end
 
   def deposit(amount)
     @balance += amount
     puts "You have deposited #{amount}."
     puts "Your current balance is #{@balance}."
+    @transactions << "#{@transactions_count += 1}. You have deposited #{amount}"
   end
 
   def withdraw(amount)
@@ -17,6 +20,7 @@ class Account
       @balance -= amount
       puts "You have withdrawn #{amount}"
       puts "Your current balance is #{@balance}."
+      @transactions << "#{@transactions_count += 1}. You have withdrawn #{amount}"
     else
       puts "dude you don't have that much money."
     end
